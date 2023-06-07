ada8443dfe9734c7d353cba5433d227575c083ae
tangxq <17693430686@163.com>
2023-06-07 17:58:08 +0800

    feat: 添加.prettierrc 格式化优化文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,24 @@
 <template>
   <view class="index">
     <view>
-      <img src="" alt="">
+      <img src="" alt="" />
     </view>
-    {{ msg }} <Dongdong />
+    {{ msg }}
+    <Dongdong />
     <view class="btn">
       <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
     </view>
-    <nut-toast :msg="msg2" v-model:visible="show" :type="type" :cover="cover"/>
+    <nut-toast :msg="msg2" v-model:visible="show" :type="type" :cover="cover" />
   </view>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
-import { Dongdong } from '@nutui/icons-vue-taro';
+import { reactive, toRefs } from 'vue'
+import { Dongdong } from '@nutui/icons-vue-taro'
 export default {
   name: 'Index',
   components: {
-    Dongdong
+    Dongdong,
   },
   setup() {
     const state = reactive({
@@ -25,27 +26,27 @@ export default {
       msg2: '你成功了～',
       type: 'text',
       show: false,
-      cover: false
-    });
+      cover: false,
+    })
 
     const handleClick = (type, msg, cover = false) => {
-      state.show = true;
-      state.msg2 = msg;
-      state.type = type;
-      state.cover = cover;
-    };
+      state.show = true
+      state.msg2 = msg
+      state.type = type
+      state.cover = cover
+    }
 
     return {
       ...toRefs(state),
-      handleClick
+      handleClick,
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss">
 .index {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
